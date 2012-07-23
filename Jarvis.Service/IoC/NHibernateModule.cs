17bea11fddd9a9d9b8b76f33f6018cd4d906d119
Andrea Fonti <andreafontict@gmail.com>
2012-07-23 23:07:36 +0200

    Added Ninject Module loading nhibernate and Application Settings

---
+++ Jarvis.Service/IoC/NHibernateModule.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using Jarvis.Service.Domain.Mappings;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using Ninject.Activation;
+using Ninject.Modules;
+using Ninject;
+namespace Jarvis.Service.IoC
+{
+    public class NHibernateModule:NinjectModule
+    {
+        #region Overrides of NinjectModule
+
+        public override void Load()
+        {
+            Bind<ISessionFactory>().ToMethod(CreateSessionFactory).InSingletonScope();
+            Bind<ISession>().ToMethod((x) => x.Kernel.Get<ISessionFactory>().OpenSession()).InThreadScope();
+        }
+
+        #endregion
+
+        private static ISessionFactory CreateSessionFactory(IContext ctx)
+        {            
+              var  sessionFactory = Fluently
+                .Configure()
+                .Database(
+                SQLiteConfiguration.Standard.UsingFile(Properties.Settings.Default.DbPath)
+                )
+                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<LocationMap>())
+                .ExposeConfiguration(BuildSchema)
+                .BuildSessionFactory();
+
+            return sessionFactory;
+        }
+
+        private static void BuildSchema(Configuration config)
+        {
+            new SchemaUpdate(config).Execute(false, true);
+        }
+    }
+}
