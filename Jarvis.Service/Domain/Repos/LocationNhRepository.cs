fc72daac3224b342d80f94368543d7e5de840021
Andrea Fonti <andreafontict@gmail.com>
2012-07-23 23:19:05 +0200

    Added interface ILocationRepository interface(it seems it's not inherited)

--- Jarvis.Service/Domain/Repos/LocationNhRepository.cs
+++ Jarvis.Service/Domain/Repos/LocationNhRepository.cs
@@ -3,7 +3,7 @@ using NHibernate;
 
 namespace Jarvis.Service.Domain.Repos
 {
-    public class LocationNhRepository : DomainModel.NhRepository<Location.Location,Guid>
+    public class LocationNhRepository : DomainModel.NhRepository<Location.Location,Guid>, ILocationRepository
     {
         public LocationNhRepository(ISession session) : base(session)
         {
