0779f0ace0596add931606d75c7b6cc643420a7b
Andrea Fonti <andreafontict@gmail.com>
2012-07-23 14:47:31 +0200

    Began testing of repository, created xaml file to load object graph to store into db

---
+++ Jarvis.Service.Test/Domain/Repos/LocationNhRepositoryTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Xaml;
+using Jarvis.Service.Domain.Location;
+using NUnit.Framework;
+
+namespace Jarvis.Service.Test.Domain.Repos
+{
+    [TestFixture]
+    public class LocationNhRepositoryTest
+    {
+        [Test]
+        public void SaveTest()
+        {
+            var locations = XamlServices.Load(@"Domain\Repos\Data.xaml") as List<Jarvis.Service.Domain.Location.Location>;
+        }
+    }
+}
