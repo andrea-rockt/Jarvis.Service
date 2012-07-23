7054a5d06c8b7fad095ae3a51e9c55268f1e58ee
Andrea Fonti <andreafontict@gmail.com>
2012-07-23 23:07:18 +0200

    Configured test fixture to bring up Nhibernate

--- Jarvis.Service.Test/Domain/Repos/LocationNhRepositoryTest.cs
+++ Jarvis.Service.Test/Domain/Repos/LocationNhRepositoryTest.cs
@@ -4,7 +4,13 @@ using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Xaml;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
 using Jarvis.Service.Domain.Location;
+using Jarvis.Service.Domain.Mappings;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
 using NUnit.Framework;
 
 namespace Jarvis.Service.Test.Domain.Repos
@@ -12,6 +18,28 @@ namespace Jarvis.Service.Test.Domain.Repos
     [TestFixture]
     public class LocationNhRepositoryTest
     {
+        private ISession _session;
+
+        [TestFixtureSetUp]
+        public void FixtureSetup()
+        {
+            var sessionFactory = Fluently
+                .Configure()
+                .Database(
+                SQLiteConfiguration.Standard.UsingFile("jarvis_test.sqlite3")
+                )
+                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<LocationMap>())
+                .ExposeConfiguration(BuildSchema)
+                .BuildSessionFactory();
+
+            _session = sessionFactory.OpenSession();
+        }
+
+        private static void BuildSchema(Configuration config)
+        {
+            new SchemaUpdate(config).Execute(false, true);
+        }
+
         [Test]
         public void SaveTest()
         {
