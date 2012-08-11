375f71e5a984e491560e8030f2916c0c7552db7e
Andrea Fonti <andreafontict@gmail.com>
2012-08-11 19:30:11 +0200

    Corrected previous commit

---
+++ Jarvis.Service/IoC/JarvisModule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Jarvis.Service.Business.Location;
+using Jarvis.Service.Business.Statistics;
+using Jarvis.Service.Domain.Repos;
+using ManagedWifi;
+using Ninject.Modules;
+
+namespace Jarvis.Service.IoC
+{
+    class JarvisModule : NinjectModule
+    {
+        #region Overrides of NinjectModule
+
+        /// <summary>
+        /// Loads the module into the kernel.
+        /// </summary>
+        public override void Load()
+        {
+            Bind<IManagedWifiContext>().To<ManagedWifiContext>();
+            Bind<ILocationProvider>().To<LocationProvider>();
+            Bind<ISensorDatasProvider>().To<WlanSensorDatasProvider>();
+            Bind<ILocationRepository>().To<LocationNhRepository>();
+            Bind<ILocationStatsRepository>().To<LocationStatsNhRepository>();
+            Bind<IStatsCollector>().To<StatsCollector>();
+        }
+
+        #endregion
+    }
+}
---
+++ Jarvis.Service/JarvisService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace Jarvis.Service
+{
+    public partial class JarvisService : ServiceBase
+    {
+        public JarvisService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+#if DEBUG
+        public void DebugOnStart(string[] args)
+        {
+            OnStart(args);
+        }
+#endif
+    }
+}
